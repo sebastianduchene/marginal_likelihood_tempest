8dcb542607a9c5ed915228fc8324a4155eed39d2
sebastian duchene <sebastianduchene@sebastians-MacBook-Air-2.local>
2019-06-25 11:20:17 +1000

    add xml files of unusual runs

---
+++ plot_probs.R
@@ -0,0 +1,47 @@
+get_mle <- function(lines){
+	split_lines <- strsplit(grep('generalized stepping', lines, value = T), ' ')[[1]]
+	return(as.numeric(split_lines[length(split_lines)]))
+}
+
+
+templates <- paste0('short_sampling_high_rate/', c('GSS_strict_dates_het_rel1_SIMS',
+	  'GSS_ucld_dates_het_rel1_SIMS', 'GSS_strict_ultra_het_rel1_SIMS',
+	  'GSS_ucld_ultra_het_rel1_SIMS', 'GSS_strict_random_het_rel1_SIMS',
+	  'GSS_ucld_random_het_rel1_SIMS'), '.mle.result.log')
+
+plot(c(0,7), c(0, -1400), type = 'n', main = 'Short high rate') 
+sims <- paste0('sim_', 1:10)
+
+for(i in 1:length(sims)){
+      res_files <- gsub('SIMS', sims[i], templates)
+      mles <- vector()
+      for(j in 1:length(res_files)){
+      	    mles[j] <- get_mle(readLines(res_files[j]))
+      }
+      offset <- rnorm(1, 0, 0.1)
+      lines(1:length(mles)+offset, mles-max(mles), type = 'l')
+}
+#############
+#############
+
+templates <- paste0('bd_dates_low_rate/', c('GSS_strict_dates_utlra_clock_SIMS',
+	  'GSS_ucld_dates_utlra_clock_SIMS', 'GSS_strict_ultra_utlra_clock_SIMS',
+	  'GSS_ucld_ultra_utlra_clock_SIMS', 'GSS_strict_random_utlra_clock_SIMS',
+	  'GSS_ucld_random_utlra_clock_SIMS'), '.mle.result.log')
+
+plot(c(0,7), c(0, -10), type = 'n', main = 'bd low rate') 
+sims <- paste0('sim_', 1:10)
+
+for(i in 1:length(sims)){
+      res_files <- gsub('SIMS', sims[i], templates)
+      mles <- vector()
+      for(j in 1:length(res_files)){
+      	    mles[j] <- get_mle(readLines(res_files[j]))
+      }
+      offset <- rnorm(1, 0, 0.1)
+      lines(1:length(mles)+offset, mles-max(mles), type = 'l')
+      if(i==1) 
+}
+#############
+#############
+
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Simulating alignment from heterochronous and isochronous trees
+
+bd_dates_high_rate: constant sampling, evol. rate=5E-3
+bd_dates_low_rate: constant sampling, evol. rate=1E-5
+short_sampling_low_rate: sampling for 1/10 of the process, evol. rate=5E-3
+short_sampling_high_rate: sampling for 1/10 of the process, evol. rate=1E-5
+
+Data sets with abnormal patterns:
+- In bd_dates_low_rate 
+GSS_strict_dates_utlra_clock_sim_1.xml	GSS_strict_ultra_utlra_clock_sim_1.xml	GSS_ucld_random_utlra_clock_sim_1.xml
+GSS_strict_random_utlra_clock_sim_1.xml	GSS_ucld_dates_utlra_clock_sim_1.xml	GSS_ucld_ultra_utlra_clock_sim_1.xml
+
+GSS_strict_dates_utlra_rel0.1_sim_6.xml	GSS_strict_ultra_utlra_rel0.1_sim_6.xml	GSS_ucld_random_utlra_rel0.1_sim_6.xml
+GSS_strict_random_utlra_rel0.1_sim_6.xml GSS_ucld_dates_utlra_rel0.1_sim_6.xml	GSS_ucld_ultra_utlra_rel0.1_sim_6.xml
+
+GSS_strict_dates_utlra_rel0.5_sim_10.xml GSS_strict_ultra_utlra_rel0.5_sim_10.xml GSS_ucld_random_utlra_rel0.5_sim_10.xml
+GSS_strict_random_utlra_rel0.5_sim_10.xml GSS_ucld_dates_utlra_rel0.5_sim_10.xml GSS_ucld_ultra_utlra_rel0.5_sim_10.xml
+
+- In short_sampling_high_rate --X Checked. This one is fine.
+GSS_strict_dates_het_rel1_sim_9.xml	GSS_strict_ultra_het_rel1_sim_9.xml	GSS_ucld_random_het_rel1_sim_9.xml
+GSS_strict_random_het_rel1_sim_9.xml	GSS_ucld_dates_het_rel1_sim_9.xml	GSS_ucld_ultra_het_rel1_sim_9.xml
+
